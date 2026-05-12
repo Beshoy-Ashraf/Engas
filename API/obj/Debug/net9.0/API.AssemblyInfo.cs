a07b356bc87d0f85378c70cf32baf367018a00a5
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2026-05-12 17:02:50 +0300

    Update AssemblyInfo with new informational version and cache hash

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0460f6b6d7c1298a179bbefa4268bcb0168cb1eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a934b6a5676a684b7abc64cd90244d54234fcb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
